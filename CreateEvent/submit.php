73b6803e4ef3a0c15ec4735437b959fb5955f394
Anagha097 <46780950+Anagha097@users.noreply.github.com>
2019-03-10 21:06:07 +0530

    Update submit.php

--- CreateEvent/submit.php
+++ CreateEvent/submit.php
@@ -6,9 +6,8 @@ $d=$_POST['etime'];
 $e=$_POST['numspeakers'];
 $f=$_POST['maxparticipants'];
 $g=$_POST['sponsoramt'];
-$h=$_POST['amtcompleted']
+$h=$_POST['amtcompleted'];
 $i=$_POST['descr'];
-$j=$_POST['organizer'];
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -20,7 +19,7 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully\n";
-$sql = "INSERT INTO `events` (`eid`,`ename`,`edate`,`etime`,`numspeakers`,`maxparticipants`,`sponsoramt`,`amtcompleted`,`descr`,`organizer`) VALUES ('$a','$b','$c','$d','$e','$f','$g','$h','$i','$j')";
+$sql = "INSERT INTO `events` (`eid`,`ename`,`edate`,`etime`,`numspeakers`,`maxparticipants`,`sponsoramt`,`amtcompleted`,`descr`) VALUES ('$a','$b','$c','$d','$e','$f','$g','$h','$i')";
 
 if ($conn->query($sql) === TRUE) {
 	echo "New record created successfully";
