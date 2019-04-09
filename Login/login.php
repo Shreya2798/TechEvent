fbfbb27eae896fc2c733f5395a7ab7169cde9132
Anagha097 <46780950+Anagha097@users.noreply.github.com>
2019-04-09 20:00:21 +0530

    Update login.php

--- Login/login.php
+++ Login/login.php
@@ -3,33 +3,33 @@ session_start();
 if (isset($_POST['submit']) ){
     
 $conn=mysqli_connect('localhost','root','');
-mysqli_select_db($conn,'authentication');
-
+mysqli_select_db($conn,'db1');
 $username = $_POST['username'];
 $pass = $_POST['password'];
-$pass_db='';
-$query="SELECT password FROM users WHERE username='$username'";
-
-
-if(!empty($pass_db=mysqli_query($conn,$query)))
+//$pass_db='';
+$message='';
+$query="SELECT password FROM users WHERE username='$username' and password='$pass'";
+$pass_db=mysqli_query($conn,$query);
+//$_SESSION['password']=$pass_db;
+if(isset($pass_db) )
 {
-    if($pass == $pass_db)
-    {
+   // if($pass == $pass_db)
+    
         $_SESSION['message']="Registration success";
-        $_SESSION['username']=$username;
-        header("location: cong.html"); //replace the file accordingle
+        $_SESSION['username0']=$username;
+		//$_SESSION['password']=$pass_db;
+        header("location: organizer.php"); //replace the file accordingle
     }
     else{
        $_SESSION['message']="Invalid Password";
+	   
     }
-}
 
-else{
+/*else{
     $_SESSION['message']="Invalid Username";
     
-}
+}*/
 mysqli_close($conn);
-
 }
 ?>
 
@@ -47,7 +47,7 @@ mysqli_close($conn);
 <body>
 
 <div class="signin">
-<form method="post">
+<form method="post" >
 <h3 style="color:#fff;">Log In</h3>
 <p style="font-size:10px;" color='red'>   
     <?php
@@ -61,12 +61,12 @@ mysqli_close($conn);
 <input type="password" name="password" placeholder="Password" required class="inputtype" /> <br /><br />
 <input type="submit" name="submit" value="Log In"/> <br/> <br/>
 
-<div id="container">
+<!--<div id="container">
 <a href="resetPass.html" style=" margin-right:0px; font-size:13px; font-family:Tahoma, Geneva, sans-serif;">Reset password?</a>
 <a href="forgotPass.html" style=" margin-left:30px; font-size:13px; font-family:Tahoma, Geneva, sans-serif;">Forget password</a>
     </div><br /><br /><br /><br /><br /><br />
 Don't have account?<a href="register.php" style="font-family:'Play', sans-serif;">&nbsp;Sign Up</a>
-
+-->
 </form>
 </div>
 
