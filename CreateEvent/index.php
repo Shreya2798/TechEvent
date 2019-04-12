dcab7310bd9e86c92411dc02702b6fcd99932aab
Shreya C N <40518886+Shreya2798@users.noreply.github.com>
2019-04-13 01:05:47 +0530

    Update index.php

--- CreateEvent/index.php
+++ CreateEvent/index.php
@@ -1,30 +1,63 @@
 <!DOCTYPE html>
 <html>
-<body>
+<head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-<form action="submit.php" method="post">
-	Event ID:</br>
-	<input type="text" name="eid"></br></br>
-	Event Name:</br>
-	<input type="text" name="ename"></br></br>
-	Event Date:</br>
-	<input type="text" name="edate"></br></br>
-	Event Time:</br>
-	<input type="text" name="etime"></br></br>
-	Number of Speakers:</br>
-	<input type="text" name="numspeakers"></br></br>
-	Maximum No. of Participants:</br>
-	<input type="text" name="maxparticipants"></br></br>
-	Sponsor Amount:</br>
-	<input type="text" name="sponsoramt"></br></br>
-	Amount Completed:</br>
-	<input type="text" name="amtcompleted"></br></br>
-	Event Description:</br>
-	<input type="text" name="descr"></br></br>
-	<input type="submit" value="Submit">
-</form>
+<style>
+body  {
+	background-position:center;
+	background-size:595px 540px;
+    background-image :url(blue.jpg);
+    background-repeat:no-repeat;
+}
+.form-div{
+	margin:auto;
+	border:3px solid blue;
+	width:590px;
+}
+.btnSubmit
+{
+    border:none;
+    border-radius:1.5rem;
+    padding:1%;
+    width:20%;
+    cursor:pointer;
+    background:#0062cc;
+    color:#fff;
+}
+.form-control {
+	width:400px;
+	border:3px solid #ced4da;
+	border-radius:1.5rem;
+	
+}
+.form-control1{
+	border:3px solid #ced4da;
+	border-radius:1.5rem;
+}
+</style>
+</head>
+<body class="container">
 
+
+            <div class="form-div">
+                <h2 style="text-align:center" >Create Your Event</h2>
+<form action="submit.php" method="post" >
+	<pre>
+	<b>Event Name:				Username:				</br>
+	<input type="text" name="ename" class="form-control1" >		<input type="text" name="username" class="form-control1" >	 </br></br>
+	Event Date:                        Event Time:</br>
+	<input type="text" name="edate" class="form-control1" >		<input type="text" name="etime" class="form-control1" ></br></br>
+	Number of Speakers:                Maximum No. of Participants:</br>
+	<input type="text" name="numspeakers" class="form-control1" >		<input type="text" name="maxparticipants" class="form-control1" ></br></br>
+	sponsor Amount:                    Amount Completed:</br>
+	<input type="text" name="sponsoramt" class="form-control1"  >		<input type="text" name="amtcompleted" class="form-control1" ></br></br>
+	Event Description:</br>
+	<input type="text" name="descr" class="form-control" ></br></br>
+	<input type="submit" value="Submit"  class="btnSubmit" >
+</pre>
+</div>
+</form>
 </body>
 </html>
