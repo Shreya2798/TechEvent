85e4bb7271ad183ff145ec201f177d3efec5cb11
likitha0710 <46808428+likitha0710@users.noreply.github.com>
2019-04-21 09:22:20 +0530

    Update reject.php

--- Login/reject.php
+++ Login/reject.php
@@ -6,10 +6,10 @@ session_start();
 
     $query = "DELETE FROM `requests` WHERE `requests`.`eid` = '$eid' and `Sender` = '$id';";
         if(performQuery($query)){
-            echo "Account has been rejected.";
+            echo '<h3 style="color:red;">Account has been rejected. </h3>';
         }else{
-            echo "Unknown error occured. Please try again.";
+            echo '<h3 style="color:red;">Unknown error occured. Please try again.</h3>';
         }
 ?>
 <br/><br/>
-<a href="Notification.php">Home</a>
+<a href="Notification.php">Go to home</a>
