52e30d586cffd01b193337785d02d54206ed84ab
Shreya C N <40518886+Shreya2798@users.noreply.github.com>
2019-04-25 00:19:30 +0530

    Update submit.php

--- PlanAheadFinal/submit.php
+++ PlanAheadFinal/submit.php
@@ -1,29 +1,29 @@
 <?php
-//$a=$_POST['eid'];
 $b=$_POST['ename'];
 $c=$_POST['edate'];
 $d=$_POST['etime'];
 $e=$_POST['numspeakers'];
 $f=$_POST['maxparticipants'];
 $g=$_POST['sponsoramt'];
-$h=$_POST['amtcompleted'];
 $i=$_POST['descr'];
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "authentication";
+$dbname = "techevent";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully\n";
-$sql = "INSERT INTO `events` (`ename`,`edate`,`etime`,`numspeakers`,`maxparticipants`,`sponsoramt`,`amtcompleted`,`descr`) VALUES ('$b','$c','$d','$e','$f','$g','$h','$i')";
+$sql = "INSERT INTO `events` (`ename`,`edate`,`etime`,`numspeakers`,`maxparticipants`,`sponsoramt`,`descr`) VALUES ('$b','$c','$d','$e','$f','$g','$i')";
+
 if ($conn->query($sql) === TRUE) {
-	echo '<script>alert("New record created successfully");</script>';
-    //sleep(1000);
-    header("location: organiser.php");
+	echo"<h2>Successfully Created</h2>";
+echo "<script>setTimeout(\"location.href = '../index2.php';\",1500);</script>";
 } else {
 	echo "Error: " . $sql . "</br>" . $conn->error;
 }
+
 $conn->close();
 ?>
