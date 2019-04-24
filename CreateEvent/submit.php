8800a23ccede278a31fb32054cf56cdb550f5342
Shreya C N <40518886+Shreya2798@users.noreply.github.com>
2019-04-25 00:18:11 +0530

    Update submit.php

--- CreateEvent/submit.php
+++ CreateEvent/submit.php
@@ -9,18 +9,18 @@ $i=$_POST['descr'];
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "db1";
+$dbname = "techevent";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully\n";
 $sql = "INSERT INTO `events` (`ename`,`edate`,`etime`,`numspeakers`,`maxparticipants`,`sponsoramt`,`descr`) VALUES ('$b','$c','$d','$e','$f','$g','$i')";
 
 if ($conn->query($sql) === TRUE) {
-	echo "New record created successfully";
+	echo"<h2>Successfully Created</h2>";
+echo "<script>setTimeout(\"location.href = '../index2.php';\",1500);</script>";
 } else {
 	echo "Error: " . $sql . "</br>" . $conn->error;
 }
